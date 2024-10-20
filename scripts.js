20a9b702c2aa09f9d54bcd4d22c60fba5274d92c
mkuch <kuchimpos@g.ucla.edu>
2024-10-20 16:50:28 -0700

    Create color functionality for Etch-A-Sketch
    Squares that are hovered over will be covered
    black

--- scripts.js
+++ scripts.js
@@ -8,6 +8,18 @@ const cols = container.children;
 
 for(let col of cols){
   for(let i = 0; i < 16; i++){
-    col.appendChild(document.createElement("div"));
+    col.appendChild(document.createElement("div")).classList.add("square");
   }
 }
+
+const squares = document.querySelectorAll(".square");
+
+squares.forEach((square) => {
+  square.addEventListener("mouseenter", (e) => {
+    e.target.classList.add("colored");
+  });
+
+  square.addEventListener("mouseleave", (e) => {
+    setTimeout( () => e.target.classList.remove("colored"), 3000);
+  });
+});
